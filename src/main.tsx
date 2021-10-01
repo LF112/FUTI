e54918acae0afaf51475ca14d2f1781eefc7d2a3
futiwolf <lf@lf112.net>
2021-10-01 09:49:32 +0800

    ✨ 增加 console log 徽章

--- src/main.tsx
+++ src/main.tsx
@@ -20,3 +20,14 @@ ReactDOM.render(
 	</React.StrictMode>,
 	document.getElementById('FUTIWOLF')
 )
+
+if (process.env.NODE_ENV === 'production') {
+	console.clear()
+	console.log(
+		'\n %c \u26a1futiwolf %c https://www.futiwolf.com %c BY%c LF112  \n\n',
+		'color: #ffffff; background: rgb(0, 145, 228); padding:5px 0;',
+		'background:rgba(197, 197, 197, 0.89); padding:5px 0;',
+		'color: #ffffff; background: rgba(49, 49, 49, 0.85); padding:5px 0;',
+		'color: rgb(0, 145, 228); background: rgba(49, 49, 49, 0.85); padding:5px 0;'
+	)
+}
