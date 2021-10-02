971bf3f2a98be2e51654ac2ce983837bffc30667
futiwolf <lf@lf112.net>
2021-10-02 11:29:09 +0800

    ⚡️ 转换 Live2D 关键依赖为 chunk

--- src/components/Live2D/index.tsx
+++ src/components/Live2D/index.tsx
@@ -8,8 +8,8 @@ export default (props: any) => {
 	const node = useRef<HTMLDivElement>()
 
 	//=> 懒加载 Live2D 相关组件
-	const _emscriptenModule = import.meta.globEager('../../libs/Emscripten/*.js')
-	const _live2dCoreModule = import.meta.globEager('../../libs/*.js')
+	const _emscriptenModule = import.meta.glob('../../libs/Emscripten/*.js')
+	const _live2dCoreModule = import.meta.glob('../../libs/*.js')
 	const _live2dFrameworkModule = import.meta.glob('../../libs/framework/*.js')
 	useEffect(() => {
 		//=> 定义全局常量
@@ -19,20 +19,23 @@ export default (props: any) => {
 		// Emscripten Core Module
 		if (typeof globalThis._em !== 'function')
 			for (const path in _emscriptenModule) {
-				const KEY = Object.keys(_emscriptenModule[path])[0]
-				globalThis._em = _emscriptenModule[path][KEY]()
+				_emscriptenModule[path]().then(mod => {
+					const KEY = Object.keys(mod)[0]
+					globalThis._em = mod[KEY]()
+				})
 			}
 
 		// Live2D Core
 		if (typeof globalThis.Live2DCubismCore !== 'function')
 			for (const path in _live2dCoreModule) {
-				const KEY = Object.keys(_live2dCoreModule[path])[0]
-				console.log(_live2dCoreModule[path][KEY], '\n', _live2dCoreModule)
+				_live2dCoreModule[path]().then(mod => {
+					const KEY = Object.keys(mod)[0]
 
-				//=> 定义 Live2D Core 全局常量
-				globalThis.Live2DCubismCore = _live2dCoreModule[path][KEY]
-				globalThis.LIVE2DCUBISMCORE = _live2dCoreModule[path][KEY]
-				//↑ Live2D Framework 前置依赖 ↑
+					//=> 定义 Live2D Core 全局常量
+					globalThis.Live2DCubismCore = mod[KEY]
+					globalThis.LIVE2DCUBISMCORE = mod[KEY]
+					//↑ Live2D Framework 前置依赖 ↑
+				})
 			}
 		// Live2D Framework
 		if (
