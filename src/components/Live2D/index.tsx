4a59736285a8d43727ad131390ab30be249e8308
futiwolf <lf@lf112.net>
2021-10-10 10:15:29 +0800

    🧐 Live2d sdk 初装载测试

--- src/components/Live2D/index.tsx
+++ src/components/Live2D/index.tsx
@@ -6,8 +6,15 @@ import { useUpdateModel3, useModel3 } from 'state/live2d/hooks'
 import { Live2DState } from 'state/live2d/slice'
 //[ state ]
 
-import { downloadModel } from 'utils/getLive2dModel'
+import { downloadModel, CubismLogFn } from 'utils/getLive2d'
 //[ utils ]
+
+import {
+	CubismFramework,
+	Option,
+	LogLevel
+} from 'libs/live2dFramework/src/live2dcubismframework'
+//[ libs ]
 
 //=> DOM
 export default (props: any) => {
@@ -17,12 +24,29 @@ export default (props: any) => {
 	const model3 = useModel3().model3
 
 	useEffect(() => {
+		//=> Main
 		if (model3.Version == 0) {
 			const fetchModelData = async () =>
 				updateModel3((await downloadModel()) as Live2DState)
 
 			fetchModelData()
-		} else console.log(model3)
+		} else {
+			console.log(model3)
+
+			//=> 装载 Cubism SDK
+			// 配置 Cubism SDK
+			CubismFramework.startUp({
+				logFunction: CubismLogFn, // 日志输出方法
+				loggingLevel: LogLevel.LogLevel_Verbose // 日志级别
+			}) // 'Cubism SDK 其实没啥可配置的，大无语'
+			// 初始化 cubism SDK
+			CubismFramework.initialize()
+
+			return () => {
+				//=> 释放 Cubism SDK 实例
+				CubismFramework.dispose()
+			}
+		}
 	}, [model3])
 
 	return (
