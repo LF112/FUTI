dc1478e07f20faf95ebb1f7e9f2b25731f944217
futiwolf <lf@lf112.net>
2021-10-17 08:21:04 +0800

    ✨ 新增触摸提示

--- src/components/HeadPic/index.tsx
+++ src/components/HeadPic/index.tsx
@@ -91,6 +91,11 @@ export default (props: any) => {
 					)}
 					{l2dShow ? LIVE2D : <></>}
 				</IMGCentered>
+				<TouchMe>
+					<i className='el-icon-thumb'>
+						<span>TOUCH</span>
+					</i>
+				</TouchMe>
 				<Frame>
 					<div></div>
 					<nav></nav>
@@ -214,4 +219,43 @@ const LoadMask = styled.div`
 	display: flex;
 	align-items: center;
 	justify-content: center;
+`
+
+const TouchMe = styled.div`
+	position: absolute;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	padding: 8px;
+	display: flex;
+	align-items: flex-end;
+	justify-content: flex-end;
+	> i {
+		color: #fff;
+		font-size: 20px;
+		font-weight: lighter;
+		padding: 4px 6px;
+		border-radius: 4px;
+		user-select: none;
+		text-align: center;
+		display: inline-grid !important;
+		> span {
+			color: #fff;
+		}
+	}
+	animation: 1s linear infinite;
+	animation-name: breathe;
+	animation-timing-function: ease-in-out;
+	animation-direction: alternate;
+	@keyframes breathe {
+		0% {
+			opacity: 0.4;
+			color: hsla(0, 0%, 100%, 0);
+		}
+		100% {
+			opacity: 0.9;
+			color: #fff;
+		}
+	}
 `
