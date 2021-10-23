b4caaa0fc60bc07d95f2061d1c54bb622f236d76
futiwolf <lf@lf112.net>
2021-10-23 23:51:07 +0800

    ⚡️ 懒加载 GA1 背景

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, Suspense } from 'react'
 
 //import Loading from 'page/loading/main'
-import GA1 from 'components/Background/GA1'
 import Header from 'page/header'
 import Main from 'page/main'
 import Footer from 'page/footer'
@@ -21,6 +20,12 @@ export default () => {
 		setTimeout(() => LoadAn(() => updateLoadStatus(true)), 500)
 	}, [])
 
+	//=> 懒加载背景组件 GA1
+	const ga1Module = import.meta.glob('./components/Background/GA1.tsx')
+	const LazyGA1 = React.lazy(
+		ga1Module['./components/Background/GA1.tsx'] as any
+	)
+
 	return (
 		<>
 			<Popups />
@@ -29,7 +34,7 @@ export default () => {
 			<Main />
 			<Footer />
 
-			<GA1 />
+			<LazyGA1 />
 		</>
 	)
 }
--- src/components/Background/GA1.tsx
+++ src/components/Background/GA1.tsx
@@ -1,25 +1,47 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import styled from 'styled-components'
 import Parallax from 'parallax-js'
+import fastdom from 'fastdom'
 
 //=> DOM
 export default (props: any) => {
+	const node = useRef<HTMLDivElement>()
 	const container = useRef<HTMLDivElement>()
+
+	const [_backGroundLeft, _setBackGroundLeft] = useState<string>('')
+	const [_backGroundRight, _setBackGroundRight] = useState<string>('')
+	const LEFTURL = 'https://cdn.lfio.net/background/background_left_560.png'
+	const RIGHTURL = 'https://cdn.lfio.net/background/background_right_560.png'
 
 	useEffect(() => {
 		new Parallax(container.current)
+
+		const LEFTDOM = new Image()
+		const RIGHTDOM = new Image()
+		LEFTDOM.src = LEFTURL
+		RIGHTDOM.src = RIGHTURL
+
+		LEFTDOM.onload = () => _setBackGroundLeft(LEFTURL)
+		RIGHTDOM.onload = () => _setBackGroundRight(RIGHTURL)
+
 		return () => Parallax.disable()
 	}, [container])
 
+	useEffect(() => {
+		if (_backGroundRight !== '' && _backGroundLeft !== '')
+			fastdom.measure(() => {
+				const DOM = node.current.style
+				fastdom.mutate(() => {
+					DOM.opacity = '1'
+				})
+			})
+	}, [_backGroundLeft, _backGroundRight])
+
 	return (
-		<GA1>
+		<GA1 ref={node as any}>
 			<div ref={container as any}>
-				<LEFT data-depth='0.2' />
-				<RIGHT
-					data-depth='0.3'
-					src='https://cdn.lfio.net/background/background_right_560.png'
-					alt='background'
-				/>
+				<LEFT data-depth='0.2' src={_backGroundLeft} alt='background' />
+				<RIGHT data-depth='0.3' src={_backGroundRight} alt='background' />
 			</div>
 		</GA1>
 	)
@@ -33,6 +55,8 @@ const GA1 = styled.div`
 	top: 0;
 	left: 0;
 	z-index: -1;
+	opacity: 0;
+	transition: all 0.2s cubic-bezier(0.22, 0.58, 0.12, 0.98) !important;
 	> div {
 		display: flex;
 		align-items: center;
@@ -40,13 +64,11 @@ const GA1 = styled.div`
 		filter: blur(5px);
 	}
 `
-const LEFT = styled.div`
+const LEFT = styled.img`
 	top: -25px !important;
 	left: -35vw !important;
 	height: calc(100vh + 50px);
 	width: 35vw;
-	background-image: url('https://cdn.lfio.net/background/background_left_560.png');
-	background-size: cover;
 	transition: all 0.5s cubic-bezier(0.22, 0.58, 0.12, 0.98);
 `
 
@@ -55,6 +77,5 @@ const RIGHT = styled.img`
 	height: calc(100vh + 90px);
 	top: -60px !important;
 	left: 30vw !important;
-	position: relative;
 	transition: all 0.5s cubic-bezier(0.22, 0.58, 0.12, 0.98);
 `
