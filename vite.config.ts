fecf863043e7428c54177dd9e88fd2474bd463c6
futiwolf <lf@lf112.net>
2021-11-08 10:18:50 +0800

    ✨ gzip 支持

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,10 @@ import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import svgr from 'vite-plugin-svgr'
+import viteCompression from 'vite-plugin-compression'
 
 export default defineConfig({
-	plugins: [tsconfigPaths(), reactRefresh(), svgr()],
+	plugins: [tsconfigPaths(), reactRefresh(), svgr(), viteCompression()],
 	server: {
 		port: 10240
 	}
