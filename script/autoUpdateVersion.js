e05e8838915ff1182123ac1adf26c26ff8b71347
futiwolf <lf@lf112.net>
2021-11-27 11:28:18 +0800

    🐛 更变为 es6

--- script/autoUpdateVersion.js
+++ script/autoUpdateVersion.js
@@ -3,8 +3,8 @@ Auto Update Version
 xx.xx.xx-xxxxx
 */
 
-const fs = require('fs')
-const { exec } = require('child_process')
+import { access as exists, constants, readFile } from 'fs'
+import { exec } from 'child_process'
 
 const command = (cmd, options, callback) => {
 	console.log('\n')
@@ -13,39 +13,46 @@ const command = (cmd, options, callback) => {
 }
 const args = process.argv.slice(2)
 
-fs.exists('./package.json', exists => {
-	if (exists) {
-		const { version: versionCurrent } = require('../package.json')
+exists('./package.json', constants.F_OK | constants.W_OK, err => {
+	if (!err) {
+		readFile('./package.json', (err, data) => {
+			if (err) console.error('文件读取失败')
+			else {
+				const versionCurrent = JSON.parse(data).version
 
-		const V1 = 25 // 0.0.x 最大值
-		const V2 = 20 // 0.x.0 最大值
+				const V1 = 25 // 0.0.x 最大值
+				const V2 = 20 // 0.x.0 最大值
 
-		console.log('\n', ` 正在自动更新版本号 (Version：${versionCurrent})`)
-		let versionArr = versionCurrent.split('-')[0].split('.').map(Number)
+				console.log(versionCurrent)
 
-		if (versionArr[2] == V1) {
-			versionArr[2] = 0
-			if (versionArr[1] == V2) {
-				versionArr[1] = 1
-				versionArr[0]++
-			} else versionArr[1]++
-		} else versionArr[2]++
+				console.log('\n', ` 正在自动更新版本号 (Version：${versionCurrent})`)
+				let versionArr = versionCurrent.split('-')[0].split('.').map(Number)
 
-		const versionNew = `${versionArr.join('.')}-${args[0].split('=')[1]}`
+				if (versionArr[2] == V1) {
+					versionArr[2] = 0
+					if (versionArr[1] == V2) {
+						versionArr[1] = 1
+						versionArr[0]++
+					} else versionArr[1]++
+				} else versionArr[2]++
 
-		if (versionNew !== versionCurrent) {
-			// 更新 package.json version
-			command(
-				`npm --no-git-tag-version version ${versionNew}`,
-				{},
-				(error, stdout, stderr) => {
-					if (!error) console.log(`版本号更新成功！Version: ${versionNew}`)
-					else
-						console.log(
-							chalk.yellow(`\n 更新版本号（${versionNew}）失败了～\n`)
-						) || process.exit(1)
-				}
-			)
-		} else console.log(`\n 本次版本号未做修改，Version: ${versionNew} ！`)
+				const versionNew = `${versionArr.join('.')}-${args[0].split('=')[1]}`
+
+				if (versionNew !== versionCurrent) {
+					// 更新 package.json version
+					command(
+						`npm --no-git-tag-version version ${versionNew}`,
+						{},
+						(error, stdout, stderr) => {
+							if (!error) console.log(`版本号更新成功！Version: ${versionNew}`)
+							else
+								console.log(
+									chalk.yellow(`\n 更新版本号（${versionNew}）失败了～\n`)
+								) || process.exit(1)
+						}
+					)
+				} else console.log(`\n 本次版本号未做修改，Version: ${versionNew} ！`)
+			}
+		})
 	} else console.log(' 仓库不存在！')
 })
