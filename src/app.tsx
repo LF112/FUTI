3a60f91b4258a321993709422ee2bdf9bc125b70
LF112 <lf@lf112.net>
2025-04-16 15:48:09 +0800

    feat: footer

--- src/app.tsx
+++ src/app.tsx
@@ -2,12 +2,15 @@ import React from 'react';
 
 import { ScrollArea } from '@/components/ui/scroll-area';
 
+import { Footer } from '@/components/page/footer.tsx';
 import { Header } from '@/components/page/header.tsx';
 
 export const App: React.FC = () => {
   return (
     <ScrollArea className="futi-ui h-dvh w-dvw min-w-max [&>[data-radix-scroll-area-viewport]>div]:!flex [&>[data-radix-scroll-area-viewport]>div]:h-full [&>[data-radix-scroll-area-viewport]>div]:flex-col">
       <Header />
+      <main className="flex flex-1 flex-col"></main>
+      <Footer />
     </ScrollArea>
   );
 };
